93ab5fd220555f8c6b34e0bb3b59440490b38838
bootstrap-prime <bootstrap.prime@gmail.com>
2021-07-01 13:51:47 -0400

    update env for better rust dev experience

--- shell.nix
+++ shell.nix
@@ -9,14 +9,22 @@ mkShell {
     clippy
     rustfmt
     rustc
+    rustracer
+    rust-analyzer
   ];
 
   CARGO_NET_GIT_FETCH_WITH_CLI = "true";
+  # courtesy of https://discourse.nixos.org/t/rust-src-not-found-and-other-misadventures-of-developing-rust-on-nixos/11570/10
+  RUST_SRC_PATH = "${rust.packages.stable.rustPlatform.rustLibSrc}";
+  RACER_RUST_SRC_PATH = "${rust.packages.stable.rustPlatform.rustLibSrc}";
+  #RACER_CMD = "${pkgs.rustracer}/bin/racer";
 
-  shellHook = ''
-    export INSTANCE_TEMPLATE_DIR="$(pwd)/templates";
-    export PATH="$(pwd)/target/debug/instance:$PATH";
-  '';
+  # shellHook = ''
+  #   echo "${rust.packages.stable.rustPlatform.rustLibSrc}"
+  #   echo "${rustracer}"
+  # '';
 
-  #INSTANCE_TEMPLATE_DIR = /home/bootstrap/projects/instance/templates;
+  # shellHook = ''
+  #   export PATH="${pkgs.cargo}/bin/cargo:$PATH";
+  # '';
 }
